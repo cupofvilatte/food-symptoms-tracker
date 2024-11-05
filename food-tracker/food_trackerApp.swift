7f08199b76273b8125fbbb6cd9a8ee429b48d863
Vilate <viknapp22@gmail.com>
2024-11-05 10:05:22 -0700

    Initial Commit

---
+++ food-tracker/food_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  food_trackerApp.swift
+//  food-tracker
+//
+//  Created by Vilate Jules Knapp on 11/5/24.
+//
+
+import SwiftUI
+
+@main
+struct food_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ food-trackerTests/food_trackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  food_trackerTests.swift
+//  food-trackerTests
+//
+//  Created by Vilate Jules Knapp on 11/5/24.
+//
+
+import Testing
+@testable import food_tracker
+
+struct food_trackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
