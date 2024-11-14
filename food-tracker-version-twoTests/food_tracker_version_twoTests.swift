0c2d7231179babf641324c90dc488b545f8553ec
Vilate <viknapp22@gmail.com>
2024-11-14 10:35:05 -0700

    added back deleted files

---
+++ food-tracker-version-twoTests/food_tracker_version_twoTests.swift
@@ -0,0 +1,17 @@
+//
+//  food_tracker_version_twoTests.swift
+//  food-tracker-version-twoTests
+//
+//  Created by Vilate Jules Knapp on 11/14/24.
+//
+
+import Testing
+@testable import food_tracker_version_two
+
+struct food_tracker_version_twoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
