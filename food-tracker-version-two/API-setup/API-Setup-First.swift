b1379e10067eebe5d468e4250d266b551743492b
sophia62 <sophiabeebe1@gmail.com>
2024-11-26 10:18:06 -0700

    API setup, waiting for key and document to be created

--- food-tracker-version-two/API-setup/API-Setup-First.swift
+++ food-tracker-version-two/API-setup/API-Setup-First.swift
@@ -1,4 +1,5 @@
 //
+
 //  API-Setup-First.swift
 //  food-tracker-version-two
 //
@@ -6,3 +7,26 @@
 //
 
 import Foundation
+
+func fetchFilms(completionHandler: @escaping ([Food]) -> Void) {
+    let url = URL(string: domainUrlString + "films/")!
+
+    let task = URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
+      if let error = error {
+        print("Error with fetching Food: \(Food Not Found)")
+        return
+      }
+
+      guard let httpResponse = response as? HTTPURLResponse,
+            (200...299).contains(httpResponse.statusCode) else {
+        print("Error with the response, unexpected status code: \(response)")
+        return
+      }
+
+      if let data = data,
+        let filmSummary = try? JSONDecoder().decode(FilmSummary.self, from: data) {
+        completionHandler(FoodSummary.results ?? [])
+      }
+    })
+    task.resume()
+  }
