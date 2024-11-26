a9c02b2f4ca0d61eec21fa449b0fa16e7e4ad5b8
sophia62 <sophiabeebe1@gmail.com>
2024-11-26 10:21:35 -0700

    Commit API changes

--- food-tracker-version-two/API-setup/API-Setup-First.swift
+++ food-tracker-version-two/API-setup/API-Setup-First.swift
@@ -7,24 +7,33 @@
 //
 
 import Foundation
+//Where to put the API and connnect to rest of code
 
-func fetchFilms(completionHandler: @escaping ([Food]) -> Void) {
-    let url = URL(string: domainUrlString + "films/")!
+func fetchFood(completionHandler: @escaping ([Food]) -> Void) {
+    let url = URL(string: domainUrlString + "food/")!
+    
+    //more code here once API is fully integrated
 
+    
+    //URL to API insert later
     let task = URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
       if let error = error {
         print("Error with fetching Food: \(Food Not Found)")
         return
       }
+        
+        //error handeling
 
       guard let httpResponse = response as? HTTPURLResponse,
             (200...299).contains(httpResponse.statusCode) else {
         print("Error with the response, unexpected status code: \(response)")
         return
       }
+        
+        //rewrite this after integraded.
 
       if let data = data,
-        let filmSummary = try? JSONDecoder().decode(FilmSummary.self, from: data) {
+        let foodSummary = try? JSONDecoder().decode(foodSummary.self, from: data) {
         completionHandler(FoodSummary.results ?? [])
       }
     })
