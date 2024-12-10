91e6f2544ffcd7484e4a0440b4a26d66906953be
Vilate <viknapp22@gmail.com>
2024-12-10 11:33:04 -0700

    integrated webview for homepage

--- food-tracker-version-two/MainPageView.swift
+++ food-tracker-version-two/MainPageView.swift
@@ -6,14 +6,29 @@
 //
 
 import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let filePath = Bundle.main.path(forResource: "index", ofType: "html") {
+            let fileURL = URL(fileURLWithPath: filePath)
+            uiView.loadFileURL(fileURL, allowingReadAccessTo: fileURL)
+        } else {
+            print("HTML file not found")
+        }
+    }
+}
+
 
 struct MainPageView: View {
     var body: some View {
         VStack {
-            Text("Welcome to the Main Page!")
-                .font(.largeTitle)
-                .padding()
-            // Add more content for your main page here
+            WebView()
+                .frame(height: 300) // Adjust frame size as needed
         }
     }
 }
